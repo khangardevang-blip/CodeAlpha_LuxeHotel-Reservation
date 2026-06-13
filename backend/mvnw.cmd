43f9109a7a6e93888ddc77f220d061d2fb307bb7
khangardevang-blip <khangardevang@gmail.com>
2026-06-13 12:42:37 +0530

    Add files via upload

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -27,6 +27,8 @@
 @REM   MVNW_VERBOSE - true: enable verbose log; others: silence the output
 @REM ----------------------------------------------------------------------------
 
+set JAVA_HOME=C:\Program Files\Java\jdk-25
+
 @IF "%__MVNW_ARG0_NAME__%"=="" (SET __MVNW_ARG0_NAME__=%~nx0)
 @SET __MVNW_CMD__=
 @SET __MVNW_ERROR__=
